3b85ea0bad7d3a3f1205e25564e534b25efe9510
Ignacio Rivas <rivasign@gmail.com>
2017-06-27 13:54:02 +0200

    Support sss files (#9)

--- src/utils.js
+++ src/utils.js
@@ -37,7 +37,7 @@ export const isSingleFile = (path: string): boolean => {
 }
 
 export const getFiles = (cwd: string, componentName?: string): string[] => {
-  const extensions = '{js,ts,jsx,tsx,css,less,scss,json}'
+  const extensions = '{js,ts,jsx,tsx,css,less,scss,sss,json}'
   const pattern = componentName ? `**/${componentName}{.,.*.}${extensions}` : `**/*.${extensions}`
   return glob.sync(pattern, { cwd, absolute: true, nodir: true })
 }
