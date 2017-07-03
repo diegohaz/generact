f940dd67967510bc42bf03715cae994d55e6bb63
Dennis Hellweg <github@dennis-hellweg.de>
2017-07-03 17:30:59 +0200

    added docs

--- index.d.ts
+++ index.d.ts
@@ -4,16 +4,36 @@ type InquirerFile = {
   value: string;
 };
 
-export declare function getComponentName(path: string): string
+/**
+ * Get the folder of a component at `path`.
+ *
+ * @param path Path to a component.
+ * @return The folder of the component given at `path`.
+ */
 export declare function getComponentFolder(path: string): string
-export declare function isSingleFile(path: string): boolean
-export declare function getFiles(cwd: string, componentName?: string): string[]
+
+/**
+ * Search for React components in directory `root`.
+ *
+ * @param root Directory to use as root.
+ * @param workingDir (Optional) Directory the command is executed from (used for relative paths).
+ * @return A promise resolving to an array of objects containing information about found components.
+ */
 export declare function getComponentFiles(
   root: string,
   workingDir?: string,
 ): Promise<InquirerFile[]>
+
+/**
+ * Replicate component given by `originialPath` into component with name `answers.name`
+ * in folder `answers.folder`.
+ *
+ * @param originalPath Path of the component to replicate.
+ * @param answers An object containing `name` and `folder` of the new component.
+ * @param workingDir (Optional) Current working directory
+ */
 export declare function performReplication(
-  path: string,
+  originalPath: string,
   answers: { name: string; folder: string },
   workingDir?: string,
 ): Promise<void>
